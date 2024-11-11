cd3f70568b93c63167be003ed369071edbdf9747
Neoshrimp <shadow638@protonmail.com>
2024-11-11 19:27:44 +0200

    add API cs files

---
+++ NoMetaScaling/Core/API/ExposedStatics.cs
@@ -0,0 +1,23 @@
+﻿using LBoL.EntityLib.Cards.Neutral.Green;
+using LBoL.EntityLib.Cards.Neutral.Red;
+using NoMetaScaling.Core.EnemyGroups;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NoMetaScaling.Core.API
+{
+    internal static class ExposedStatics
+    {
+        internal static Dictionary<string, SummonerInfo> summonerInfo = new Dictionary<string, SummonerInfo>();
+
+        internal static HashSet<string> splitableSE_CARD_ids = new HashSet<string>() { nameof(RangziFanshu), nameof(MeihongPower) };
+
+
+        internal static HashSet<string> dontBanUnlessCopied = new HashSet<string>();
+
+        internal static HashSet<string> exemptFromBan = new HashSet<string>();
+
+
+    }
+}
---
+++ NoMetaScaling/Core/API/NoMetaScalinAPI.cs
@@ -0,0 +1,49 @@
+﻿using LBoLEntitySideloader;
+using NoMetaScaling.Core.EnemyGroups;
+using NoMetaScalling;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NoMetaScaling.Core.API
+{
+    public class NoMetaScalinAPI
+    {
+        /// <summary>
+        /// GUID of the plugin.
+        /// </summary>
+        public const string GUID = PInfo.GUID;
+
+        /// <summary>
+        /// Limits how many summoned units will drop resources (P, Money, points) upon death.
+        /// </summary>
+        /// <param name="summonerId">Unit which is doing the summoning.</param>
+        /// <param name="summonLimit">Number of summons which will drop resources.</param>
+        public static void AddOrOverwriteSummoner(string summonerId, int summonLimit) => ExposedStatics.summonerInfo.AlwaysAdd(summonerId, new SummonerInfo(summonerId, summonLimit));
+
+
+        /// <summary>
+        /// If a <i>stackable</i> Status Effect provides meta resources (i.e. Sweet Potato) it will be split into two stacks.
+        /// One will track the amount added by legitimate cards, the other the amount added by banned cards.
+        /// </summary>
+        /// <param name="se_CARD_ID">Id of a CARD which adds the Status Effect in question.</param>
+        public static void SelectivelyBanMetaScalingSatusEffect(string se_CARD_ID) => ExposedStatics.splitableSE_CARD_ids.Add(se_CARD_ID);
+
+        /// <summary>
+        /// Exempt a card from a ban if it was generated in combat. 
+        /// Should only be applied to specific token(non-pooled) cards. 
+        /// The card is still banned after being played.
+        /// </summary>
+        /// <param name="cardId">Card Id to exempt.</param>
+        public static void ExemptFromBanIfGenerated(string cardId) => ExposedStatics.exemptFromBan.Add(cardId);
+
+
+
+        /// <summary>
+        /// Same as ExemptFromBanIfGenerated but Copies of the card will be banned.
+        /// </summary>
+        /// <param name="cardId">Card Id to exempt.</param>
+        public static void ExemptFromBanIfGeneratedUnlessCopied(string cardId) => ExposedStatics.dontBanUnlessCopied.Add(cardId);
+
+    }
+}

03778573ff296653dc91a91c28e618e081a0749a
Neoshrimp <shadow638@protonmail.com>
2024-12-14 16:20:24 +0200

    Fix Spirits not providing meta resources in rare cases.

---
+++ NoMetaScaling/Core/Exceptions/SpiritBuff_Patches.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using HarmonyLib;
+using LBoL.EntityLib.StatusEffects.Enemy;
+using NoMetaScaling.Core.Trackers;
+
+namespace NoMetaScaling.Core.Exceptions
+{
+
+
+    [HarmonyPatch(typeof(ShenlingHp), nameof(ShenlingHp.OnEnemyDied))]
+    class ShenlingHp_Patch
+    {
+        static void Prefix(ShenlingHp __instance)
+        {
+            ARTracker.lastActionSource = __instance;
+        }
+    }
+
+
+    [HarmonyPatch]
+    class ShenlingGold_Patch
+    {
+
+        static IEnumerable<MethodBase> TargetMethods()
+        {
+            yield return AccessTools.FirstMethod(typeof(ShenlingGold), mi => mi.Name.Contains("<OnAdded>"));
+        }
+
+        static void Prefix(ShenlingGold __instance)
+        {
+            ARTracker.lastActionSource = __instance;
+        }
+    }
+
+}
--- NoMetaScaling/README.md
+++ NoMetaScaling/README.md
@@ -76,6 +76,8 @@ It's defined in [`NoMetaScalinAPI`](https://github.com/Neoshrimp/LBoL_Doremy/blo
 *Change log*
 
 ------
+`1.1.7` Fix Spirits not providing meta resources in rare cases.
+
 `1.1.6` Exempt Dubious Tea Party.
 
 `1.1.5` Add a small safeguard.
